f9202d3a13fc343bce96b747b2c9a7cc17181c4a
shahd <shahdahmed1444@gmail.com>
2025-07-08 22:07:29 -0700

    Added screenshot and updated README

--- src/QuantumBookstoreFullTest.java
+++ src/QuantumBookstoreFullTest.java
@@ -3,14 +3,9 @@ public class QuantumBookstoreFullTest {
     public static void main(String[] args) {
 
         Inventory inv = new Inventory();
-        inv.addBook(new PaperBook("101", "Python Course", "Ali",
-                                  2012, 150.0, 3));
-
-        inv.addBook(new EBook("202", "Clean Code", "Shahd",
-                              2020, 200.0, "PDF"));
-
-        inv.addBook(new ShowcaseBook("303", "Java Course", "Unknown",
-                                     1946, 0.0));
+        inv.addBook(new PaperBook("101", "Python Course", "Ali",2012, 150.0, 3));
+        inv.addBook(new EBook("202", "Clean Code", "Shahd",2020, 200.0, "PDF"));
+        inv.addBook(new ShowcaseBook("303", "Java Course", "Unknown",1946, 0.0));
 
         System.out.println("------------------------------------------------------------");
 
